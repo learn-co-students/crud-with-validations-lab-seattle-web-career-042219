59daa4831b59ea3a44ef3aa836f3b21d4626a593
github username <github email address>
2019-05-22 20:23:09 -0700

    Done.

---
+++ db/migrate/seed.rb
@@ -0,0 +1,3 @@
+Song.create(:title => "Test song 1", :genre => "Test genre 1", :released => true, :release_year => 998, :artist_name => "test artist 1")
+Song.create(:title => "Test song 2", :genre => "Test genre 2", :released => true, :release_year => 998, :artist_name => "test artist 2")
+Song.create(:title => "Test song 3", :genre => "Test genre 3", :released => true, :release_year => 998, :artist_name => "test artist 3")
