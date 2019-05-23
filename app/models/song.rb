5ec67bdcf8e6237b3ed730e1d2797d7192f96568
Danny Tseng <github email address>
2019-05-22 17:50:35 -0700

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,15 @@
+class Song < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  validate :year_released
+
+  private
+  def year_released
+    if released
+      if release_year == nil
+        errors.add(:release_year, "Please specified the year of release.")
+      elsif release_year > Time.now.year
+        errors.add(:release_year, "Enter a valid release year.")
+      end
+    end
+  end
+end
